2bfff8fefdd6720402aaedacb5f8710d0805fa39
Hendrik Gani <hendrik.gani@gmail.com>
2018-04-06 17:13:06 +1000

    add comment

--- ganilib/Classes/App/Device.swift
+++ ganilib/Classes/App/Device.swift
@@ -2,6 +2,8 @@
 public class Device {
     public static let id = UIDevice.current.identifierForVendor!.uuidString
     
+    // Apparently these only return the selected value if that particular language is supported by the app.
+    // If not, it will fallback to the default language.
     public static let longLang = Locale.current.identifier
     public static let shortLang = Locale.current.languageCode
 }
