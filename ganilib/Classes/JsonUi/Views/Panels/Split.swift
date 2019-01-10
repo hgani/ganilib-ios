7181c5f37cedf34d83e4b87042b0031d78ce0946
Hendrik Gani <hendrik.gani@gmail.com>
2019-01-10 19:45:21 +1100

    set sensible defaults

--- ganilib/Classes/JsonUi/Views/Panels/Split.swift
+++ ganilib/Classes/JsonUi/Views/Panels/Split.swift
@@ -1,18 +1,21 @@
 class JsonView_Panels_SplitV1: JsonView {
-    private let panel = GSplitPanel()
+    private let panel = GSplitPanel().width(.matchParent)
 
     override func initView() -> UIView {
         let content = spec["content"]
         return panel.withViews(
-            createSubview(content["left"]),
-            createSubview(content["center"]),
-            createSubview(content["right"])
+            createSubview(content["left"], stretch: false),
+            createSubview(content["center"], stretch: true),
+            createSubview(content["right"], stretch: false)
         )
     }
 
-    private func createSubview(_ subviewSpec: Json) -> UIView {
+    private func createSubview(_ subviewSpec: Json, stretch: Bool) -> UIView {
         if subviewSpec.null != nil {
-            return UIView()
+            if stretch {
+                return GView()
+            }
+            return GView().width(0)
         }
         return JsonView.create(spec: subviewSpec, screen: screen)?.createView() ?? UIView()
     }
